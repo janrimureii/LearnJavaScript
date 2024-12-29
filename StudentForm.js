039558c4256af7a9e1f63bd4cf25e159ecf444f8
janrimureii <jnergaminghd@gmail.com>
2024-12-29 18:23:15 +0800

    naayos ko na tho meron pang bug na onti

--- StudentForm.js
+++ StudentForm.js
@@ -1,36 +1,39 @@
+//empty arrays
 let studentNames = [];
 let studentCourse = [];
 
-const nametb = document.getElementById("stud-name");
-const programtb = document.getElementById("stud-program");
-const button = document.getElementById("submit");
-
+//table data
 let names = document.getElementById("student-names");
 let course = document.getElementById("student-program");
 
-function pushstudname() {
-    names.innerHTML = nametb.value;
-}
+//Form
+const nametb = document.getElementById("stud-name");
+const programtb = document.getElementById("stud-program");
+const submit = document.getElementById("submit");
+const displaystudent = document.getElementById("display");
 
-button.addEventListener('click',() => {
-    studentNames.push(nametb);
-    alert(studentNames);
+//Adding a value inside the array
+submit.addEventListener('click',() => {
+  studentNames.unshift(nametb.value);
+  studentCourse.unshift(programtb.value);
+    alert('ADDED');
+});
+
+//displaying the array
+display.addEventListener('click', () => {
+    studentNames.forEach(items => {
+        let line1 = document.createElement("p");
+        line1.textContent = items;
+        names.appendChild(line1);
+    });
+    
+    studentCourse.forEach(items => {
+        let line2 = document.createElement("p");
+        line2.textContent = items;
+        course.appendChild(line2);
+    });
+
+    alert("Display");
 });
 
 
-
-
-
-document.getElementById("text").innerHTML="Welcome back students";
-
-studentNames.forEach(items => {
-    let line1 = document.createElement("p");
-    line1.textContent = items;
-    names.appendChild(line1);
-});
-
-studentCourse.forEach(items => {
-    let line2 = document.createElement("p");
-    line2.textContent = items;
-    course.appendChild(line2);
-});
